5e66d589f3a588a18488afd0defb5c87d57b7efa
shellieD <shellie.downie13@gmail.com>
2022-03-30 23:07:02 +0000

    Amend typo

--- taskmanager/models.py
+++ taskmanager/models.py
@@ -3,7 +3,7 @@ from taskmanager import db
 
 class Category(db.Model):
     # schema for the Category model
-    id = db.Column(db.Integer, prinary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     category_name = db.Column(db.String(25), unique=True, nullable=False)
     tasks = db.relationship("Task", backref="category", cascade="all, delete", lazy=True)
 
